67de9aa4e068a9023f40b3158e7e53c5b33c670f
Jayant-Jeet <jayant.jeet.tomar.me@gmail.com>
2025-03-22 06:24:51 +0530

    Using useState

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,10 @@
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
 
 function ListGroup() {
     let listItems = [] as string[];
     listItems = ['City1', 'City2', ' City3', 'City4', 'City5'];
-    let selectedIndex = -1;
-
-    const handleClick = (event: MouseEvent) => {
-        if (selectedIndex > -1) {
-            document.querySelector('.active')?.classList.remove('active');
-        }
-        selectedIndex = listItems.indexOf(event.currentTarget.textContent as string
-        );
-        event.currentTarget.classList.add('active');
-    }
+    // Hook
+    const [slectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
@@ -20,8 +12,8 @@ function ListGroup() {
         <ul className="list-group">
             {listItems.length>0 ? listItems.map((item, index) => (
                 <li key={index} 
-                onClick={handleClick}
-                className="list-group-item">{item}</li>
+                onClick={(e: MouseEvent) => setSelectedIndex(index)}
+                className={index === slectedIndex ? "list-group-item active" : "list-group-item"}>{item}</li>
             )) : <li className="list-group-item">No items</li>}
         </ul>
     </>
